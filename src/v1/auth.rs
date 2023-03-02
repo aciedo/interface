afb5ecc9762f969aada6f6c9d8f0b798ed009f94
Azz <azz@valera.co>
2023-03-02 14:09:20 +0000

    move from platform

---
+++ src/v1/auth.rs
@@ -0,0 +1,78 @@
+use rkyv::{Deserialize, Archive, Serialize, CheckBytes};
+use uuid::Uuid;
+use webauthn_proto::{CreationChallengeResponse, RegisterPublicKeyCredential, RequestChallengeResponse, PublicKeyCredential};
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct RegisterNumberReq {
+    /// The name of the user to register.
+    pub name: String,
+    /// The phone number to register.
+    pub number: String,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct RegisterNumberRes {
+    /// A value to multiply the code by for VerifyNumberReq
+    pub multiplier: i64,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct VerifyNumberReq {
+    pub number: String,
+    pub code: i64,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct VerifyNumberRes {
+    /// The user's profile id
+    pub id: Uuid,
+    /// WebAuthn challenge
+    pub opts: CreationChallengeResponse,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct RegisterPasskeyReq {
+    /// The profile id
+    pub id: Uuid,
+    /// The challenge
+    pub challenge: Vec<u8>,
+    /// The response from the browser
+    pub response: RegisterPublicKeyCredential,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct AuthChallengeReq {
+    /// The profile id
+    pub id: Uuid,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct AuthChallengeRes {
+    /// WebAuthn challenge
+    pub opts: RequestChallengeResponse,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct AuthVerifyReq {
+    /// The profile id
+    pub id: Uuid,
+    /// The challenge
+    pub challenge: Vec<u8>,
+    /// The response from the browser
+    pub response: PublicKeyCredential,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct AuthVerifyRes {
+    /// The session ID to use for future requests
+    pub id: Uuid,
+}
---
+++ src/v1/profile.rs
@@ -0,0 +1,60 @@
+use rkyv::{Archive, Deserialize, Serialize, CheckBytes};
+#[cfg(feature = "scylla")]
+use scylla::{FromRow, ValueList};
+use uuid::Uuid;
+
+// only derive ValueList and FromRow if we're not compiling for wasm
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[cfg_attr(feature = "scylla", derive(FromRow, ValueList))]
+#[archive_attr(derive(CheckBytes))]
+pub struct Profile {
+    /// The user's UUID
+    pub id: Uuid,
+    pub avatar: Option<String>,
+    /// hash(hash(phone) + pepper)
+    /// This is always 32 bytes long.
+    pub hashed_phone: Vec<u8>,
+    pub name: String,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct GetProfileByIdReq {
+    pub id: Uuid,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct GetProfileByIdRes {
+    pub profile: Option<Profile>,
+}
+
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct UploadContactsReq {
+    pub contacts: Vec<Vec<u8>>,
+}
+
+/// A [Contact] contains additional, user-provided information about someone.
+/// The person may or _may not_ be a registered user of the service. Users can
+/// optionally sync their contacts using end-to-end encryption.
+/// This allows them to see the names of their contacts across devices.
+///
+/// Users choose a device to be the "master" device, which will be used to
+/// sync the contacts. This is set by default to the device that was used
+/// to create the account and doesn't change unless the user explicitly
+/// changes it.
+///
+/// None of the fields on this pub struct are visible to the server.
+#[derive(Archive, Deserialize, Serialize, Debug, Clone)]
+#[archive_attr(derive(CheckBytes))]
+pub struct Contact {
+    /// The person's name, loosely formatted.
+    pub name: String,
+    /// The person's optional nickname.
+    pub nickname: Option<String>,
+    /// The person's phone number.
+    pub phone: String,
+    /// The person's optional avatar, JPEG encoded. Max size is 256KiB.
+    pub avatar: Option<Vec<u8>>,
+}
